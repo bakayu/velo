f66b8381a10b720d9081aecdec01998be7abf86d
Ayush Chauhan <56398566+bakayu@users.noreply.github.com>
2025-11-20 13:07:09 +0530

    feat: add subscriber logic (#1)
    * feat: add GET /health_check endpoint
    
    Signed-off-by: Ayush <mail@ayuch.dev>
    
    * chore: remove config.toml
    
    Signed-off-by: Ayush <mail@ayuch.dev>
    
    * wip: POST /subscribe endpoint
    
    Signed-off-by: Ayush <mail@ayuch.dev>
    
    * feat: implement persistent storage with postgres
    
    Signed-off-by: Ayush <mail@ayuch.dev>
    
    * chore: update sqlx offline data
    
    Signed-off-by: Ayush <mail@ayuch.dev>
    
    * Update sqlx cache for test targets
    
    * tests: create a new database for each test run
    
    When running tests, we want to ensure that each test has a clean database environment.
    
    Signed-off-by: Ayush <mail@ayuch.dev>
    
    ---------
    
    Signed-off-by: Ayush <mail@ayuch.dev>

---
+++ src/startup.rs
@@ -0,0 +1,23 @@
+use crate::routes::{health_check, subscribe};
+use actix_web::dev::Server;
+use actix_web::{App, HttpServer, web};
+use sqlx::PgPool;
+use std::net::TcpListener;
+
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(db_pool);
+    let server = HttpServer::new(move || {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscribe", web::post().to(subscribe))
+            .app_data(db_pool.clone())
+    })
+    .listen(listener)?
+    .run();
+
+    println!("\n{}", "=".repeat(60));
+    println!("\n\tServer running at : http://127.0.0.1:8000\n");
+    println!("{}\n", "=".repeat(60));
+
+    Ok(server)
+}
---
+++ tests/health_check.rs
@@ -0,0 +1,133 @@
+use sqlx::{Connection, Executor, PgConnection, PgPool};
+use std::net::TcpListener;
+use uuid::Uuid;
+use velo::configuration::{DatabaseSettings, get_configuration};
+use velo::startup::run;
+
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
+/// Spin up an instance of our application and return its address
+/// (e.g. http://127.0.0.1:XXXX)
+async fn spawn_app() -> TestApp {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to a random port");
+    let port = listener.local_addr().unwrap().port();
+    let address = format!("http://127.0.0.1:{}", port);
+
+    let mut configuration = get_configuration().expect("Failed to read the configuration file");
+    configuration.database.database_name = Uuid::new_v4().to_string();
+
+    let connection_pool = configure_database(configuration.database).await;
+
+    let server = run(listener, connection_pool.clone()).expect("Failed to bind address");
+
+    drop(tokio::spawn(server));
+
+    TestApp {
+        address,
+        db_pool: connection_pool,
+    }
+}
+
+pub async fn configure_database(config: DatabaseSettings) -> PgPool {
+    // Create database
+    let mut connection = PgConnection::connect(&config.connection_string_without_db())
+        .await
+        .expect("Failed to create database");
+
+    connection
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database.");
+
+    // Migrate database
+    let connection_pool = PgPool::connect(&config.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+
+    connection_pool
+}
+
+#[tokio::test]
+async fn health_check_test() {
+    // Arrange
+    let test_app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get(format!("{}/health_check", &test_app.address))
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let test_app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    // Act
+    let body = "name=Jon%20Doe&email=mail%40jondoe.com";
+    let response = client
+        .post(format!("{}/subscribe", &test_app.address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute post request");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&test_app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "mail@jondoe.com");
+    assert_eq!(saved.name, "Jon Doe");
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_for_missing_data() {
+    // Arrange
+    let app_addr = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=Jon%20Doe", "missing the email"),
+        ("email=mail%40jondoe.com", "missing the name"),
+        ("", "missing both entries"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let responses = client
+            .post(format!("{}/subscribe", &app_addr.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute post request");
+
+        // Assert
+        assert_eq!(
+            400,
+            responses.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
